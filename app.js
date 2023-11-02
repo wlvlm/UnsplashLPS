2d3c26c83edb1090bd805076f361ea8866f52a60
William Vuillaume <williampgm77@gmail.com>
2023-11-02 15:52:01 +0100

    Init grille et titre dynamique

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ import ACCESS_KEY from "./config.js"
 
 const form = document.querySelector('.form')
 const input = document.querySelector('.input')
-const img = document.querySelectorAll('.target-image')
+const imgContainer = document.querySelector('.imgContainer')
+const input2 = document.querySelector('.input-2')
 
 form.addEventListener("submit", (e) => {
     e.preventDefault()
@@ -28,7 +29,34 @@ async function fetchData(value) {
 }
 
 function displayData(data){
-    data.results.forEach((element, index) => {
-        img[index].src = element[index].urls.regular
-    })
+    imgContainer.textContent = '';
+    console.log(data.results)
+
+    for (let i = 0; i < 8; i++) {
+        const imgWrapper = document.createElement("div")
+        // const newImg = document.createElement("img")
+        const title = document.createElement("h3")
+
+        // newImg.src = data.results[i].urls.regular
+        title.textContent = data.results[i].alt_description
+
+        imgContainer.appendChild(imgWrapper)
+        // imgWrapper.appendChild(newImg)
+        imgWrapper.appendChild(title)
+        
+        imgWrapper.style.background = `url(${data.results[i].urls.regular})`
+        imgWrapper.style.backgroundSize = 'cover'
+        imgWrapper.style.width = '40vw'
+        imgWrapper.style.aspectRatio = '1'
+        imgWrapper.style.display = 'flex'
+        imgWrapper.style.justifyContent = 'center'
+        imgWrapper.style.alignItems = 'center'
+
+        title.style.textTransform = 'uppercase'
+        title.style.color = 'white'
+        title.style.fontFamily = 'arial'
+        title.style.fontWeight = '800'
+        title.style.fontSize = '1.5rem'
+
+    }
 }
